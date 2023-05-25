9f44da26a586f1a53b8a714a0e0bf264dd58d0e3
AndreyKapus <kapustnikov@ukr.net>
2023-05-25 17:56:36 +0300

    persist

--- src/Redux/Auth/AuthSlice.js
+++ src/Redux/Auth/AuthSlice.js
@@ -12,6 +12,6 @@ const authSlice = createSlice({
     extraReducers: {
 
     }
-})
+});
 
 export default authSlice.reducer
--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -1,6 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import authReducer from '../Auth';
 
 const persistConfig = {
@@ -13,7 +22,13 @@ const persistedUserReducer = persistReducer(persistConfig, authReducer)
 export const store = configureStore({
   reducer: {
     auth: persistedUserReducer,
-  }
+  },
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
 export const persistor = persistStore(store)
