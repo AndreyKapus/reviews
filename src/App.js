0d6d56c4c97a578cd166e6f0414fa08ec589b0a2
AndreyKapus <kapustnikov@ukr.net>
2023-05-25 14:57:03 +0300

    home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import AppBar from "./Components/AppBar/AppBar";
+import HomePage from "./Pages/Home/HomePage";
 
 
 function App() {
   return (
     <div>
+      <AppBar/>
       <Routes>
-        <Route path="/" element={<AppBar/>}/>
+        <Route path="/" element={<HomePage/>}/>
       </Routes>
     </div>
   );
---
+++ src/Components/AppBar/RegisterForm/RegisterForm.js
@@ -0,0 +1,16 @@
+const RegisterForm = () => {
+    return (
+        <div>
+            <form>
+                <label>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <button type="submit">Sign up</button>
+                </label>
+            </form>
+        </div>
+    )
+};
+
+export default RegisterForm;
