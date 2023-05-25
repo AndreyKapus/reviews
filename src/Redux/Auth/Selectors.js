263c1e0f1dc01b94d0b03e224e99ea54fb80f6c5
AndreyKapus <kapustnikov@ukr.net>
2023-05-25 18:24:47 +0300

    selectors

---
+++ src/Redux/Auth/Selectors.js
@@ -0,0 +1,9 @@
+const getIsLogedIn = state => state.auth.isLogedIn;
+const getUserName = state => state.auth.user.name;
+
+const authSelectors = {
+    getIsLogedIn,
+    getUserName,
+};
+
+export default authSelectors;
