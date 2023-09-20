6128899a5a33405dae8aa30c4414a0f2cfce135c
AndreyKapus <kapustnikov@ukr.net>
2023-09-20 12:27:33 +0300

    fix

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,42 +1,81 @@
 import { useState } from "react";
 import AddModal from "../AddReviewModal/AddReviewModal";
-import {AddWrapper, ReviewsList, ReviewListItem, Vacancy, ReviewsWrapper, VacancyValue, DeleteBtn, SeachForm} from './Reviews.styled'
-import {VscChromeClose} from 'react-icons/vsc'
-// import * as API from "../../Services/ContactsApi"
-// import authSelectors from "../../Redux/Auth/Selectors";
-// import { useSelector } from "react-redux";
+import {
+  AddWrapper,
+  ReviewsList,
+  ReviewListItem,
+  Vacancy,
+  ReviewsWrapper,
+  VacancyValue,
+  DeleteBtn,
+  SeachForm,
+} from "./Reviews.styled";
+import { VscChromeClose } from "react-icons/vsc";
 
+const AllReviews = ({
+  reviews,
+  onDeleteReview,
+  getAllReviews,
+  page,
+  total,
+}) => {
+  const [inputValue, setInputValue] = useState("");
 
-const AllReviews = ({reviews, onDeleteReview, getAllReviews, page, total}) => {
-    const [inputValue, setInputValue] = useState('');
+  const searchReview = (e) => {
+    setInputValue(e.target.value);
+  };
 
-    const searchReview = (e) => {
-        setInputValue(e.target.value);
-    };
+  const filteredReviews =
+    inputValue !== ""
+      ? total.filter((review) =>
+          review.company.toLowerCase().includes(inputValue.toLowerCase())
+        )
+      : reviews;
 
-    const filteredReviews = inputValue !== '' && total !== [] ? total.filter(review => 
-        review.company.toLowerCase().includes(inputValue.toLowerCase())
-     ) : reviews;  
-   
-    return(
-        <ReviewsWrapper>
-            <AddWrapper>
-                <AddModal reviews={reviews} total={total} getAllReviews={getAllReviews} page={page}/>
-                <SeachForm type="text" onChange={searchReview} placeholder="search by company"/>
-            </AddWrapper>
-            <ReviewsList>
-                {reviews ? filteredReviews.map(({_id, name, company, link, date}) => {
-                   return <ReviewListItem key={_id}>
-                        <Vacancy>Vacancy: <VacancyValue>{name}</VacancyValue></Vacancy>
-                        <Vacancy>Company: <VacancyValue>{company}</VacancyValue></Vacancy>
-                        <Vacancy>Link: <VacancyValue>{link}</VacancyValue></Vacancy>
-                        <Vacancy>Date: <VacancyValue>{date}</VacancyValue></Vacancy>
-                        <DeleteBtn type="button" onClick={() => onDeleteReview(_id)}><VscChromeClose color="white" size='9px'/></DeleteBtn>
-                   </ReviewListItem>
-                }) : <div>No contacts</div>}
-            </ReviewsList>
-                
-        </ReviewsWrapper>
-)};
+  return (
+    <ReviewsWrapper>
+      <AddWrapper>
+        <AddModal
+          reviews={reviews}
+          total={total}
+          getAllReviews={getAllReviews}
+          page={page}
+        />
+        <SeachForm
+          type="text"
+          onChange={searchReview}
+          placeholder="search by company"
+        />
+      </AddWrapper>
+      <ReviewsList>
+        {reviews ? (
+          filteredReviews.map(({ _id, name, company, link, date }) => {
+            return (
+              <ReviewListItem key={_id}>
+                <Vacancy>
+                  Vacancy: <VacancyValue>{name}</VacancyValue>
+                </Vacancy>
+                <Vacancy>
+                  Company: <VacancyValue>{company}</VacancyValue>
+                </Vacancy>
+                <Vacancy>
+                  Link: <VacancyValue>{link}</VacancyValue>
+                </Vacancy>
+                <Vacancy>
+                  Date: <VacancyValue>{date}</VacancyValue>
+                </Vacancy>
+                <DeleteBtn type="button" onClick={() => onDeleteReview(_id)}>
+                  <VscChromeClose color="white" size="9px" />
+                </DeleteBtn>
+              </ReviewListItem>
+            );
+          })
+        ) : (
+          <div>No contacts</div>
+        )}
+      </ReviewsList>
+    </ReviewsWrapper>
+  );
+};
 
-export default AllReviews
+export default AllReviews;
--- src/Pages/ReviewsPage/ReviewsPage.js
+++ src/Pages/ReviewsPage/ReviewsPage.js
@@ -1,93 +1,102 @@
 import AllReviews from "../../Components/Reviews/Reviews";
-import { useEffect, useState } from "react"
-import * as API from "../../Services/ContactsApi"
+import { useEffect, useState } from "react";
+import * as API from "../../Services/ContactsApi";
 import authSelectors from "../../Redux/Auth/Selectors";
 import { useSelector } from "react-redux";
 import { Loader } from "../../Loader/Loader";
 import LoadMore from "../../LoadMore/LoadMore";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 
 const ReviewsPage = () => {
-    const [reviews, setReviews] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [page, setPage] = useState(1);
-    const [total, setTotal] = useState('');
-    const [lastPage, setLastPage] = useState(false);
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState("");
+  const [lastPage, setLastPage] = useState(false);
 
-    const showDeleteSuccess = () => {
-        toast.success('Review deleted', {
-            position: "top-right",
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
-      };
+  const showDeleteSuccess = () => {
+    toast.success("Review deleted", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
 
-    const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
-    const getAllReviews = async (page) => {
-            setIsLoading(true)
-            const responce = await API.getAll(page);
-            setReviews(responce.data);
-            setIsLoading(false)
+  const getAllReviews = async (page) => {
+    setIsLoading(true);
+    const responce = await API.getAll(page);
+    setReviews(responce.data);
+    setIsLoading(false);
+  };
+
+  const nextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const prevPage = () => {
+    setPage((prevPage) => prevPage - 1);
+  };
+
+  const deleteReview = async (id) => {
+    setIsLoading(true);
+    setReviews((prevReview) => prevReview.filter((item) => item._id !== id));
+    await API.deleteReview(id);
+    setIsLoading(false);
+    showDeleteSuccess();
+  };
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      return;
+    }
+    getAllReviews(page);
+  }, [isLoggedIn, page]);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      return;
+    }
+
+    const getAllReviewsLenght = async () => {
+      const responce = await API.getAllLenght();
+      setTotal(responce.data);
+      const isLastPage = Math.ceil(total.length / 9);
+
+      isLastPage === page ? setLastPage(true) : setLastPage(false);
     };
-    
-    const nextPage = () => {
-        setPage(prevPage => prevPage + 1)
-    };
+    getAllReviewsLenght();
+  }, [isLoggedIn, page, total.length]);
 
-    const prevPage = () => {
-        setPage(prevPage => prevPage - 1)
-    };
-
-    const deleteReview = async (id) => {
-        setIsLoading(true)
-        setReviews(prevReview => prevReview.filter(item => item._id !== id));
-        await API.deleteReview(id);
-        setIsLoading(false)
-        showDeleteSuccess()
-    };
-
-    useEffect(() => {
-        if(!isLoggedIn) {
-            return;
-        };
-    getAllReviews(page)
-       
-    }, [isLoggedIn, page]);
-
-    useEffect(() => {
-        if(!isLoggedIn) {
-          return;
-        };
-        
-          const getAllReviewsLenght = async () => {
-            const responce = await API.getAllLenght();
-          setTotal(responce.data);
-          const isLastPage = Math.ceil(total.length / 9)
-          
-          if(isLastPage === page) {
-            setLastPage(true)
-        };
-            if(isLastPage !== page) {
-                setLastPage(false)
-            }
-          }
-            getAllReviewsLenght()
-      }, [isLoggedIn, page, total.length])
-
-    return (
-        <>  
-            {isLoggedIn && <AllReviews reviews={reviews} page={page} total={total} onDeleteReview={deleteReview} getAllReviews={getAllReviews}/>}
-            <LoadMore nextPage={nextPage} prevPage={prevPage} reviews={reviews} total={total} page={page} lastPage={lastPage}/>
-            {isLoading &&  <Loader />}
-            <ToastContainer />
-        </>
-    )
+  return (
+    <>
+      {isLoggedIn && (
+        <AllReviews
+          reviews={reviews}
+          page={page}
+          total={total}
+          onDeleteReview={deleteReview}
+          getAllReviews={getAllReviews}
+        />
+      )}
+      <LoadMore
+        nextPage={nextPage}
+        prevPage={prevPage}
+        reviews={reviews}
+        total={total}
+        page={page}
+        lastPage={lastPage}
+      />
+      {isLoading && <Loader />}
+      <ToastContainer />
+    </>
+  );
 };
 
 export default ReviewsPage;
