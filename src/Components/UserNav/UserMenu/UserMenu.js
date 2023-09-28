d083852fa8e86dc2f1b1b39e3121def24dbf9ca0
AndreyKapus <kapustnikov@ukr.net>
2023-09-28 13:27:42 +0300

    fix

--- src/Components/UserNav/UserMenu/UserMenu.js
+++ src/Components/UserNav/UserMenu/UserMenu.js
@@ -1,19 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
 import authSelectors from "../../../Redux/Auth/Selectors";
 import { authOperations } from "../../../Redux/Auth";
-import {UserMenuGreetings, UserMenuWrapper, LogoutBtn} from './UserMenu.styled'
-import {IoIosLogOut} from 'react-icons/io'
+import {
+  UserMenuGreetings,
+  UserMenuWrapper,
+  LogoutBtn,
+} from "./UserMenu.styled";
+import { IoIosLogOut } from "react-icons/io";
 
 const UserMenu = () => {
-    const userName = useSelector(authSelectors.getUserName);
-    const isLoggedIn = useSelector(authSelectors.getIsLoggedIn)
-    const dispatch = useDispatch()
-    return (
-        <UserMenuWrapper>
-            {isLoggedIn && <UserMenuGreetings>Hello, {userName}!</UserMenuGreetings>}
-            <LogoutBtn type="button" onClick={() => dispatch(authOperations.logout())}><IoIosLogOut size='26px'/></LogoutBtn>
-        </UserMenuWrapper>
-    )
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const userName = useSelector(authSelectors.getUserName);
+  const dispatch = useDispatch();
+  return (
+    <UserMenuWrapper>
+      {isLoggedIn && <UserMenuGreetings>Hello, {userName}!</UserMenuGreetings>}
+      <LogoutBtn
+        type="button"
+        onClick={() => dispatch(authOperations.logout())}
+      >
+        <IoIosLogOut size="26px" />
+      </LogoutBtn>
+    </UserMenuWrapper>
+  );
 };
 
-export default UserMenu
+export default UserMenu;
