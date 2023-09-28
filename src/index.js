e712be24f21d735ee7564db5c925540cd4a5be80
AndreyKapus <kapustnikov@ukr.net>
2023-09-28 14:26:50 +0300

    title

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import {store, persistor} from './Redux/Store/Store'
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react'
-import { Global, ThemeProvider } from '@emotion/react';
-import { GlobalStyles } from './Styles/Global.styles';
-import {theme} from './Styles/Theme';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "./Redux/Store/Store";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Global, ThemeProvider } from "@emotion/react";
+import { GlobalStyles } from "./Styles/Global.styles";
+import { theme } from "./Styles/Theme";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <ThemeProvider theme={theme}>
-          <Global styles={GlobalStyles}/>
+            <Global styles={GlobalStyles} />
             <App />
           </ThemeProvider>
         </PersistGate>
